6a7ac088eef702ab4768f78e72a5f1dd4e79405f
Bruno Travouillon <devel@travouillon.fr>
2023-06-12 18:41:45 -0400

    Add script and config to mirror flat APT repos
    The script aptly-sync-flat.sh will read the configuration file matching
    the name given as first parameter, then create the mirrors, snapshots
    and publish a nightly version of the repositories.
    
    This script is intended to mirror flat APT repositories. (those without
    the `dists` hierarchy of directories).
    
    The configuration files are provided as examples to mirror Netdata
    product. Since Netdata split their repositories per OS family, two
    configuration files are needed: one for Debian and another for Ubuntu.

---
+++ aptly-sync-flat.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# This script manages flat repositories
+set -e
+
+CONFIG=${1}
+TAG=${2:-$(date +%Y%m%d%H%M)}
+
+if [ -f "${BASH_SOURCE%/*}/config/flat/aptly-${CONFIG}.conf" ]; then
+    source "${BASH_SOURCE%/*}/config/flat/aptly-${CONFIG}.conf"
+else
+    echo "File ${BASH_SOURCE%/*}/config/flat/aptly-${CONFIG}.conf not found."
+    exit 1
+fi
+
+ENDPOINT=${ENDPOINT:-"filesystem:mirror"}
+
+for DISTRIBUTION in ${!DISTRIBUTIONS[@]}; do
+    echo "####"
+    echo "# Aptly Mirror - $DISTRIBUTION-$PRODUCT"
+    echo "####"
+    aptly mirror show $DISTRIBUTION-$PRODUCT >/dev/null 2>&1 || \
+        aptly mirror create -filter="${FILTER}" -architectures=amd64 \
+            -with-sources=${WITH_SOURCES:-true} \
+            $DISTRIBUTION-$PRODUCT $MIRROR ./${DISTRIBUTIONS[$DISTRIBUTION]}/
+
+    echo "####"
+    echo "# Aptly update - $DISTRIBUTION-$PRODUCT"
+    echo "####"
+    aptly mirror update $DISTRIBUTION-$PRODUCT
+
+    echo "####"
+    echo "# Aptly snapshot - $DISTRIBUTION-$PRODUCT-$TAG"
+    echo "####"
+    aptly snapshot create $DISTRIBUTION-$PRODUCT-$TAG from mirror $DISTRIBUTION-$PRODUCT
+
+    echo "###"
+    echo "# Aptly publish - $DISTRIBUTION-$PRODUCT"
+    echo "###"
+    PREFIX="${PRODUCT}/nightly/${DISTRIBUTIONS[$DISTRIBUTION]}"
+    if aptly publish show $DISTRIBUTION-$PRODUCT ${ENDPOINT}:${PREFIX}; then
+        aptly publish switch $DISTRIBUTION-$PRODUCT ${ENDPOINT}:${PREFIX} $DISTRIBUTION-$PRODUCT-$TAG
+    else
+        aptly publish snapshot -distribution=$DISTRIBUTION-$PRODUCT $DISTRIBUTION-$PRODUCT-$TAG ${ENDPOINT}:${PREFIX}
+    fi
+done
